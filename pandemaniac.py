252fa68d125dd5ad1334e2d216afdc6d4ebe4777
Catrin <girls@10.40.20.246>
2020-02-22 18:37:58 -0800

    added function to output a list of nodes to use based on centrality measure inputted

--- pandemaniac.py
+++ pandemaniac.py
@@ -29,7 +29,7 @@ def load_graph(filename):
     G = nx.Graph(graph_dict)
 
     # Get rid of isolated nodes.
-    G.remove_nodes_from(nx.isolates(G))
+    G.remove_nodes_from(list(nx.isolates(G)))
 
     return G, num_players, num_seeds, unique_id
 
@@ -40,24 +40,43 @@ def load_graph(filename):
 
 
 def degree_centrality_top_k(G, num_seeds):
-    return heapq.nlargest(nx.degree_centrality(G), num_seeds)
+    return heapq.nlargest(num_seeds, nx.degree_centrality(G))
 
 
 def closeness_centrality_top_k(G, num_seeds):
-    return heapq.nlargest(nx.closeness_centrality(G), num_seeds)
+    return heapq.nlargest(num_seeds, nx.closeness_centrality(G))
 
 
 def betweenness_centrality_top_k(G, num_seeds):
-    return heapq.nlargest(nx.betweenness_centrality(G), num_seeds)
+    return heapq.nlargest(num_seeds, nx.betweenness_centrality(G))
 
+def select_node(filename, measure):
+    # Generate top k and write to file 
+    if measure == 0:
+        nodes = degree_centrality_top_k(G, num_seeds)
+    elif measure == 1:
+        nodes = closeness_centrality_top_k(G, num_seeds)
+    else:
+        nodes = betweenness_centrality_top_k(G, num_seeds)
+
+    out = ""
+    for node in nodes:
+        out += str(node) + "\n"
+    with open(.replace('json', 'txt'), 'w') as f:
+        f.write(out*50)
 
 # ------------------------------ #
 # MAIN PROGRAM                   #
 # ------------------------------ #
 
 if __name__ == "__init__":
-    if len(sys.argv) < 2:
-        print("Error: Need to specify a JSON graph filename.")
+    if len(sys.argv) < 3:
+        print("Error: Correct input is [JSON filename] [centrality_measure]")
         exit(1)
 
+
     G, num_players, num_seeds, unique_id = load_graph(sys.argv[1])
+
+    select_node(sys.arvg[1], sys.argv[2])
+
+
